bc0c0bd457b79766bfcaf03785e247ee7eb2ba97
Camilo Torres <octorresm94@gmail.com>
2019-01-02 15:54:19 -0500

    Funcionalidad básica
    Se agregan la mayoria de las funciones, ajustar erroes al realizar mas de una operación seguida

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@ var calculadora = (function(){
   var punto = document.getElementById('punto').addEventListener('click',insertarPunto);
   var teclas = document.getElementsByClassName('tecla');
   var on = document.getElementById('on').addEventListener('click',borrardisplay);
-  var valor= document.getElementById('display').innerHTML;
   var signo = document.getElementById('sign').addEventListener('click',insertarSigno);
   var contTeclas = teclas.length;
   var resultado = 0;
   var esResultado = false;
+  var operacion = '';
 
   //asignar escuchador de eventos a las teclas
   function eventTeclas(){
@@ -23,6 +23,7 @@ var calculadora = (function(){
       this.style.padding = '2px';
     };
     //Mostrar números en pantalla
+    var valor= document.getElementById('display').innerHTML;
     var txt = valor+ this.id;
     var contTxt = txt.length;
     if(esResultado){
@@ -32,7 +33,11 @@ var calculadora = (function(){
     }
     if(contTxt <= 8 && this.id.length == 1){
       if(valor == 0){
+        if(valor == '0.'){
+          document.getElementById('display').innerHTML = (valor + this.id);
+        }else{
           valor = document.getElementById('display').innerHTML = this.id;
+        }
       }else if(valor != 0){
         valor = document.getElementById('display').innerHTML = valor+ this.id;
       }
@@ -47,25 +52,31 @@ var calculadora = (function(){
 
   //Borrar display al presionar la tecla ON/C
   function borrardisplay(){
+    var valor= document.getElementById('display').innerHTML;
     valor = 0;
     resultado = 0;
     esResultado = false;
     document.getElementById('display').innerHTML = valor;
   };
 
-
+  //Insertar el punto decimal en la cifra
   function insertarPunto(){
+    var valor= document.getElementById('display').innerHTML;
     var cont = valor.length;
       if(valor.indexOf('.') == -1  && cont < 8){
-        console.log(cont);
         document.getElementById('display').innerHTML = (valor + '.');
-      };
+      }
   };
 
   function insertarSigno(){
+    var valor= document.getElementById('display').innerHTML;
     var cont = valor.length;
-    if(valor.indexOf('-') == -1 && cont < 8){
+    if(valor.indexOf('-') == -1 && cont <= 8){
+      if(valor == 0){
+        document.getElementById('display').innerHTML = valor;
+      }else{
       document.getElementById('display').innerHTML = ('-' + valor );
+      }
     }else{
       valor = valor.substring(1);
       document.getElementById('display').innerHTML = valor;
@@ -74,24 +85,24 @@ var calculadora = (function(){
 
 
   function operacionesMatematicas(){
-    var btnSuma = document.getElementById('mas');
-    var btnResta = document.getElementById('menos');
-    var btnMultiplicacion = document.getElementById('por');
-    var btnDivision = document.getElementById('dividido');
-    btnSuma.addEventListener('click',suma);
-    btnResta.addEventListener('click',resta);
-    btnMultiplicacion.addEventListener('click',multiplicacion);
-    btnDivision.addEventListener('click',division);
+    var btnSuma = document.getElementById('mas').addEventListener('click',suma);
+    var btnResta = document.getElementById('menos').addEventListener('click',resta);
+    var btnMultiplicacion = document.getElementById('por').addEventListener('click',multiplicacion);
+    var btnDivision = document.getElementById('dividido').addEventListener('click',division);
+    var btnIgual = document.getElementById('igual').addEventListener('click',igual);
 
     function suma(){
       var valor = parseInt(document.getElementById('display').innerHTML);
-      if(resultado == 0){
+      if(resultado == 0 || esResultado != true){
         resultado = valor;
         document.getElementById('display').innerHTML = '';
+        operacion = 'esSuma';
       }else{
         resultado = resultado + valor;
         document.getElementById('display').innerHTML = resultado;
         esResultado = true;
+        operacion = 'esSuma';
+        return resultado
       }
     };
 
@@ -100,13 +111,12 @@ var calculadora = (function(){
       if(resultado == 0){
         resultado = valor;
         document.getElementById('display').innerHTML = '';
+        operacion = 'esResta';
       }else{
         resultado = resultado - valor;
         document.getElementById('display').innerHTML = resultado;
+        operacion = 'esResta';
         esResultado = true;
-      }
-      if(valor == null){
-        document.getElementById('display').innerHTML = resultado;
       }
     };
 
@@ -115,10 +125,12 @@ var calculadora = (function(){
       if(resultado == 0){
         resultado = valor;
         document.getElementById('display').innerHTML = '';
+        operacion = 'esMultiplicacion';
       }else{
         resultado = resultado * valor;
         document.getElementById('display').innerHTML = resultado;
         esResultado = true;
+        operacion = 'esMultiplicacion';
       }
     };
 
@@ -127,18 +139,65 @@ var calculadora = (function(){
       if(resultado == 0){
         resultado = valor;
         document.getElementById('display').innerHTML = '';
+        operacion = 'esDivision';
       }else{
         resultado = resultado / valor;
+        document.getElementById('display').innerHTML = resultado;
+        esResultado = true;
+        operacion = 'esDivision';
+      }
+    };
+
+    function igual(){
+      var valor = parseInt(document.getElementById('display').innerHTML);
+      if(operacion == 'esSuma' ){
+        if(esResultado){
+          document.getElementById('display').innerHTML = resultado;
+          esResultado = true;
+        }else{
+          resultado = resultado + valor;
+          document.getElementById('display').innerHTML = resultado;
+          esResultado = true;
+          operacion = '';
+        }
+      }else if(operacion == 'esResta' ){
+        if(esResultado){
+          document.getElementById('display').innerHTML = resultado;
+          esResultado = true;
+        }else{
+          resultado = resultado - valor;
+          document.getElementById('display').innerHTML = resultado;
+          esResultado = true;
+          operacion = '';
+        }
+      }else if(operacion == 'esMultiplicacion' ){
+        if(esResultado){
+          document.getElementById('display').innerHTML = resultado;
+          esResultado = true;
+        }else{
+          resultado = resultado * valor;
+          document.getElementById('display').innerHTML = resultado;
+          esResultado = true;
+          operacion = '';
+        }
+      }else if(operacion == 'esDivision' ){
+        if(esResultado){
+          document.getElementById('display').innerHTML = resultado;
+          esResultado = true;
+        }else{
+          resultado = resultado / valor;
+          document.getElementById('display').innerHTML = resultado;
+          esResultado = true;
+          operacion = '';
+        }
+      }else if(operacion == ''){
         document.getElementById('display').innerHTML = resultado;
         esResultado = true;
       }
     };
   }
 
-
-
   eventTeclas();
   operacionesMatematicas();
-  agregarSigno();
   borrardisplay();
 }());
